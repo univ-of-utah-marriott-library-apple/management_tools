7047704ed8c25babab8abc45009d25d1dde5a86b
Pierce Darragh <pierce.darragh@utah.edu>
2014-04-25 18:51:31 -0600

    Pre-bundle checks
    Making sure everything is properly in place before trying to change
    things up.

--- scripts/executable_bundler.py
+++ scripts/executable_bundler.py
@@ -2,6 +2,7 @@
 
 import argparse
 import os
+import subprocess
 import sys
 
 # Check that the logging imports properly.
@@ -48,18 +49,54 @@ def setup_logger ():
 def parse_options ():
     parser = argparse.ArgumentParser(description="Bundles groups of Python files together into a single executable.")
 
-    parser.add_argument('-i', help="The input directory containing the files to be bundled.")
-    parser.add_argument('-o', help="The output file name to use.")
-    parser.add_argument('-v', '--version', action='version', version=options['long_name'] + ' ' + options['version'], help="Display version information.")
+    parser.add_argument('-i', '--input', default='', help="The input DIRECTORY containing the files to be bundled.")
+    parser.add_argument('-o', '--output', default='', help="The output FILE name to use.")
     parser.add_argument('-l', '--log', help="Set a manual logging location.")
     parser.add_argument('-n', '--no-log', action='store_true', help="Don't write logs to file.")
+    parser.add_argument('-v', '--version', action='version', version=options['long_name'] + ' ' + options['version'], help="Display version information.")
     args = parser.parse_args()
 
+    options['input'] = args.input
+    options['output'] = args.output
     options['log'] = not args.no_log
     options['log_dest'] = args.log
 
 def main ():
+    set_globals()
     parse_options()
+    setup_logger()
+
+    # Proper formatting for the input.
+    if not options['input']:
+        options['input'] = './'
+    else:
+        options['input'] = os.path.expanduser(options['input'])
+        if not os.path.isdir(options['input']):
+            logger.error("You must specify a directory for input.")
+            sys.exit(5)
+    options['input'] = os.path.abspath(options['input'])
+    if not options['input'].endswith('/'):
+        options['input'] += '/'
+
+    # Proper formatting for the output.
+    if not options['output']:
+        options['output'] = options['input'] + os.path.basename(os.path.dirname(options['input']))
+    else:
+        options['output'] = os.path.abspath(os.path.expanduser(options['output']))
+        if not os.path.isdir(os.path.dirname(options['output'])):
+            logger.error("You must specify a filename with a valid path.")
+            sys.exit(5)
+    if not os.access(os.path.dirname(options['output']), os.W_OK):
+        logger.error("You do not have permission to save to: " + options['output'])
+        sys.exit(7)
+
+    if not os.path.isfile(options['input'] + '__main__.py'):
+        logger.error("You must have a '__main__.py' file in the input directory.")
+        sys.exit(7)
+
+    logger.info("Bundling Python files in '" + options['input'] + "' to '" + options['output'] + "'")
+
+
 
 if __name__ == "__main__":
     main()
