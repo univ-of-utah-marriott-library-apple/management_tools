1902a9f48982b70829678810de1bedc1297b2fcd
Pierce Darragh <pierce.darragh@utah.edu>
2015-03-12 18:42:55 -0600

    Disk usage analyzer
    Implementing methods to make parsing disk usage information simpler
    within other Python scripts.

---
+++ management_tools/fs_analysis.py
@@ -0,0 +1,132 @@
+import subprocess
+from itertools import groupby
+
+def get_filesystems(local_only=False):
+    """
+    """
+    fs_info = get_raw_fs_info()
+    filesystem_names = [x.split(' on')[0] for x in fs_info]
+    
+    filesystems = []
+    
+    for name in filesystem_names:
+        filesystem = Filesystem(name)
+        filesystems.append(filesystem)
+    
+    return filesystems
+
+def get_fs_type(name):
+    pass
+
+def get_raw_fs_info(fs=None):
+    # Get a list of all the currently-mounted disks' names.
+    fs_info = subprocess.check_output(['sbin/mount'])
+    
+    if not fs:
+        return fs_info
+    
+    # Split the input and clean out blank lines.
+    fs_info = fs_info.split('\n')
+    fs_info = [x for x in fs_info if x]
+    # Check for the filesystem being in the list.
+    result = [x for x in fs_info if fs in x.split(' on')[0]]
+    if len(result) > 1:
+        raise RuntimeError("Too many matches for filesystem '{}'.".format(fs))
+    elif len(result) == 0:
+        raise RuntimeError("No matches for filesystem '{}'.".format(fs))
+    else:
+        return result[0]
+
+def get_raw_fs_usage(mount_point=None, include_inodes=False, kblocks=True):
+    df = ['/bin/df']
+    if not include_inodes:
+        df.append('-P')
+    if kblocks:
+        df.append('-k')
+    if mount_point:
+        df.append(mount_point)
+    df_info = subprocess.check_output(df)
+    
+    if not mount_point:
+        return df_info
+    
+    # Split the input and clean out blank lines, as well as the leading line
+    # that consists of just headers.
+    df_info = df_info.split('\n')
+    df_info = [x for x in df_info if x]
+    if len(df_info) > 2:
+        raise RuntimeError("Specified mount point '{}' exists on two filesystems.".format(mount_point))
+    headers = df_info[0]
+    info    = df_info[1]
+    
+    #FIXME Parsing this data is a headache. Consider removing options from this method
+    # so that everything will run the same every time. Hmm.
+    
+    
+    # Count up the spacing so we know what's what.
+    space_counts = [(len(list(xpart))) for x, xpart in groupby(headers) if x == ' ']
+    for i in range(len(space_counts)):
+
+class Filesystem(object):
+    def __init__(self, name):
+        self.__name          = name
+        self.__mount_point   = None
+        self.__type          = None
+        self.__kblocks       = None
+        self.__kblocks_used  = None
+        self.__kblocks_avail = None
+        self.__capacity      = None
+        self.__properties    = None
+        
+        self.update()
+    
+    def update():
+        info = get_raw_fs_info(self.name)
+        # 'info' now contains a line like:
+        #   /dev/disk1s1 on /Volumes/External (hfs, local, journaled)
+        #   ------+-----    --------+--------  -+-
+        #       name           mount point    filesystem type
+        info = info.split('on ')[1]
+        self.__mount_point, properties = info.split(' (')
+        properties = properties[:-1]
+        properties = properties.split(', ')
+        if properties[0].endswith('fs'):
+            self.__type = properties[0]
+        
+        info = get_raw_fs_usage(self.mount_point)
+        # 'info' now contains a line like:
+        #   /dev/disk1s1  100    70    30    70%   /Volumes/External
+        #   ------+-----  -+-   -+-   -+-    -+-   --------+--------
+        #        name    blocks used avail capacity   mount point
+    
+    @property
+    def name(self):
+        return self.__name if self.__name else None
+    
+    @property
+    def mount_point(self):
+        return self.__mount_point if self.__mount_point else None
+    
+    @property
+    def type(self):
+        return self.__type if self.__type else None
+    
+    @property
+    def kblocks(self):
+        return self.__kblocks if self.__kblocks else None
+    
+    @property
+    def kblocks_used(self):
+        return self.__kblocks_used if self.__kblocks_used else None
+    
+    @property
+    def kblocks_avail(self):
+        return self.__kblocks_avail if self.__kblocks_avail else None
+    
+    @property
+    def capacity(self):
+        return self.__capacity if self.__capacity else None
+    
+    @property
+    def properties(self):
+        return self.__properties if self.__properties else None
